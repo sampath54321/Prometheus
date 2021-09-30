0bb8da3aebeacbc098838cc1ef44f5804ef37dd8
sampath54321 <45085053+sampath54321@users.noreply.github.com>
2021-09-30 14:58:26 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,14 @@
+# %%
+column = input('Column Name Please: ')
+if 'id' in column:
+    print('done')
+else:
+    print('not done')
+# %%
+
+yyyy = input('Year')
+mm = input('Month')
+dd = input('Day')
+date = (f'{dd}/{mm}/{yyyy}')
+print(date)
+# %%
